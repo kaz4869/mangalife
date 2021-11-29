66b46619007d5a618596dbc441f0763d4bb1a076
飯野和輝 <iinokazuteru@MacBook-Pro-5.local>
2021-11-30 07:48:48 +0900

    ユーザーアイコンを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_024345) do
+ActiveRecord::Schema.define(version: 2021_11_29_220553) do
 
   create_table "details", force: :cascade do |t|
     t.string "genre_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_11_27_024345) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "profile"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
