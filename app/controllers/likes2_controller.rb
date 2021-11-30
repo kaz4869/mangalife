2d4f59868c6ca7694d7f0b9459d546ae4e9fa2c0
飯野和輝 <iinokazuteru@MacBook-Pro-5.local>
2021-12-01 07:53:58 +0900

    いいね機能を更新

--- app/controllers/likes2_controller.rb
+++ app/controllers/likes2_controller.rb
@@ -9,13 +9,15 @@ class Likes2Controller < ApplicationController
 	end
 	def create
 		@post = Post.find(params[:post_id])
-		user_like_count = @post.likes.where(user_id: current_user.id).count
-		if user_like_count <=9
-			like = current_user.likes.create(post_id: params[:post_id])
+		@user_like_count = @post.likes.where(user_id: current_user.id).count + 1
+		if @user_like_count <= 10
+			like = @current_user.likes.create(post_id: params[:post_id])
+		else 
 		end
 	end
 	def destroy
 		@post = Post.find(params[:post_id])
+		@user_like_count = @post.likes.where(user_id: current_user.id).count - 1
 		like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
 		like.destroy
 	end
