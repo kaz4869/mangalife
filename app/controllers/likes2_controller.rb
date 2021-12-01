25822f13c0898f5c207fa7ca94201862c70e5688
飯野和輝 <iinokazuteru@MacBook-Pro-5.local>
2021-12-01 21:40:58 +0900

    いいね機能を更新

--- app/controllers/likes2_controller.rb
+++ app/controllers/likes2_controller.rb
@@ -1,16 +1,8 @@
 class Likes2Controller < ApplicationController
-    def index
-		@post = Post.find(params[:post_id])
-		@like_count = @post.likes.count
-		@user_like_count = @post.likes.where(user_id: current_user.id).count
-		if @user_like_count >= 10
-			redirect_to root_path
-		end
-	end
 	def create
 		@post = Post.find(params[:post_id])
 		@user_like_count = @post.likes.where(user_id: current_user.id).count + 1
-		if @user_like_count <= 10
+		if @user_like_count <=100
 			like = @current_user.likes.create(post_id: params[:post_id])
 		else 
 		end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,8 @@
 class LikesController < ApplicationController
-    def index
-		@post = Post.find(params[:post_id])
-		@like_count = @post.likes.count
-		@user_like_count = @post.likes.where(user_id: current_user.id).count
-		if @user_like_count >= 10
-			redirect_to root_path
-		end
-	end
 	def create
 		@post = Post.find(params[:post_id])
 		user_like_count = @post.likes.where(user_id: current_user.id).count
-		if user_like_count <=9
+		if user_like_count <=99
 			like = current_user.likes.create(post_id: params[:post_id])
 		end
 	end
